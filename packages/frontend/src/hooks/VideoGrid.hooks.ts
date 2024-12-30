b6d6a3867920db58508b08b12ca06a22fec46c2b
Susumu Fujii <powermac.g7@gmail.com>
2024-12-30 11:08:45 +0900

    feat: divide hooks

--- packages/frontend/src/hooks/VideoGrid.hooks.ts
+++ packages/frontend/src/hooks/VideoGrid.hooks.ts
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useRef, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import PeerConnectionManager from '../modules/PeerConnectionManager';
 
-const WEBSOCKET_URL = process.env.REACT_APP_WEBSOCKET_URL || '';
+const WEBSOCKET_URL = import.meta.env.VITE_APP_SOCKET_URL || 'http://localhost:3000';
 const MAX_VIDEOS = 16;
 
 interface RemoteStreamCallback {
