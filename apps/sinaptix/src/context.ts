5cd6501375bc3c82271b21d6982955dcec2b449f
Susumu Fujii <powermac.g7@gmail.com>
2024-12-30 13:44:06 +0900

    feat: add apps/synaptix

---
+++ apps/sinaptix/src/context.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+import { Request } from "express";
+
+export interface Context {
+  prisma: PrismaClient
+  request: Request
+}
+
+export const prisma = new PrismaClient();
